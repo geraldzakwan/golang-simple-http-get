0fbd51b9fc1557a859bf86af34287a466b5f9a73
Geraldi Dzakwan <geraldi.dzakwan@gmail.com>
2021-04-28 14:09:50 +0700

    Add server code and implement Case 1: Request without parameter id

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
   "log"
   "io/ioutil"
   "encoding/json"
+  "net/http"
 )
 
 type Data struct {
@@ -16,7 +17,7 @@ type Datum struct {
   Name string `json:"name"`
 }
 
-func main() {
+func loadData() Data {
   jsonFile, err := os.Open("data.json")
   if err != nil {
     log.Println(err)
@@ -27,11 +28,46 @@ func main() {
 
   log.Println("Data is succesfully loaded")
 
-  var result Data
-  json.Unmarshal([]byte(byteValue), &result)
+  var data Data
+  json.Unmarshal([]byte(byteValue), &data)
 
-  for i := range result.Data {
-    log.Println(result.Data[i].ID)
-    log.Println(result.Data[i].Name)
+  // for i := range data.Data {
+  //   log.Println(data.Data[i].ID)
+  //   log.Println(data.Data[i].Name)
+  // }
+
+  return data
+}
+
+func dataHandler(data Data) func(w http.ResponseWriter, r *http.Request) {
+  return func(w http.ResponseWriter, r *http.Request) {
+    if r.URL.Path != "/" {
+      http.Error(w, "404 not found.", http.StatusNotFound)
+      return
+    }
+
+    if r.Method != "GET" {
+      http.Error(w, "Method is not supported.", http.StatusNotFound)
+      return
+    }
+
+    // Case 1: Request without parameter "id"
+    if _, ok := r.URL.Query()["id"]; !ok {
+      for i := range data.Data {
+        log.Println(data.Data[i].ID)
+        log.Println(data.Data[i].Name)
+      }
+    }
+  }
+}
+
+func main() {
+  data := loadData()
+
+  http.HandleFunc("/", dataHandler(data))
+
+  log.Println("Starting server at port 8080")
+  if err := http.ListenAndServe(":8080", nil); err != nil {
+    log.Fatal(err)
   }
 }
